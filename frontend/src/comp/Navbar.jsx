f6dd8f25fbe530ef1e3e6ccbc70dc2a84894efc6
vd0789 <vd.0789.cc@gmail.com>
2025-07-15 05:01:02 +0800

    7/15/2025 05:00

--- frontend/src/comp/Navbar.jsx
+++ frontend/src/comp/Navbar.jsx
@@ -1,7 +1,18 @@
+import { Container, Flex } from '@chakra-ui/react';
 import React from 'react'
 
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <Container maxW={"1200px"} py={4}>
+    <Flex 
+      h={16}
+      justifyContent={"space-between"} 
+      alignItems={"center"}
+      flexDir={{
+        base: "column",
+        sm: "row"
+      }}
+    >
+
+    </Flex>
+  </Container>
+};
